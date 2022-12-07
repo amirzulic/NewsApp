56a43e8c1f9abd9a113fb4e377660d88547fd86e
unknown <zulicic0@gmail.com>
2022-12-07 12:11:13 +0100

    Wrapped the theme provider on the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#e8d7a7'
+        },
+        secondary: {
+            main: '#f2e9ce'
+        }
+    },
+});
 
 function App() {
     return (
         <div>
-            <Navbar/>
+            <ThemeProvider theme={theme}>
+                <Navbar/>
+            </ThemeProvider>
         </div>
     );
 }
