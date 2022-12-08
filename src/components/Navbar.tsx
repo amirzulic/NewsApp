1d8ef2d857695a8def5735805352c8a86321bd5b
unknown <zulicic0@gmail.com>
2022-12-08 14:42:29 +0100

    Navbar redesign

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,8 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import Link from '@mui/material/Link';
 
 function Navbar() {
     return (
@@ -11,8 +13,13 @@ function Navbar() {
             <Box sx={{flexGrow: 1}}>
                 <AppBar position="static" color="secondary">
                     <Toolbar>
+                        <Link href="/" color="inherit">
+                            <NewspaperIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                        </Link>
                         <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                            NEWS APP
+                            <Link href="/" color="inherit" underline="none">
+                                <b>NEWS APP</b>
+                            </Link>
                         </Typography>
                         <Button color="inherit" disabled>The biggest news platform</Button>
                     </Toolbar>
