ef5e3dd7dc1edc64197b84d2bca7eb6701c07aa1
unknown <zulicic0@gmail.com>
2022-12-12 09:45:39 +0100

    Initialized backend project

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
+
+app.listen(8000, () => {
+  console.log(`Server is running on port 8000.`);
+});
