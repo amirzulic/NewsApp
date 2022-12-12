4faf5be5ca331a0c314c0181219434d7f3e03564
Amir Zulić <zulicic0@gmail.com>
2022-12-12 12:36:20 +0100

    Initial backend endpoints (#13)

---
+++ server/data.ts
@@ -0,0 +1,38 @@
+export const PAGE_REPORTS = [
+  {
+    id: 1,
+    title: 'Report 1',
+    author: 'Some author'
+  },
+  {
+    id: 2,
+    title: 'Report 2',
+    author: 'A different author'
+  },
+  {
+    id: 3,
+    title: 'Report 3',
+    author: 'John Wick'
+  },
+  {
+    id: 4,
+    title: 'Report 4',
+    author: 'Greg Oden'
+  },
+  {
+    id: 5,
+    title: 'Report 5',
+    author: 'Kylian Mbappe'
+  }
+];
+
+export const COMMENT_LIST = [
+  {
+    comment: 'Something is written here',
+    date: new Date().toDateString()
+  },
+  {
+    comment: 'This article is very interesting',
+    date: new Date().toDateString()
+  }
+];
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,7 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
+import { PAGE_REPORTS } from './data.js';
+import { COMMENT_LIST } from './data.js';
 
 const express = require('express');
 const cors = require('cors');
@@ -11,6 +13,22 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from server!' });
+});
+
+app.get('/reports', (req, res) => {
+  res.json({ reports: PAGE_REPORTS });
+});
+
+app.get('/reports/:id', (req, res) => {
+  let id = req.params.id;
+  const report = PAGE_REPORTS.find((obj) => {
+    return obj.id === parseInt(id);
+  });
+  res.json({ report: report });
+});
+
+app.get('/comments', (req, res) => {
+  res.json({ comments: COMMENT_LIST });
 });
 
 app.listen(8000, () => {
