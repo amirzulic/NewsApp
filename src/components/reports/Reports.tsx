2b99a15fbd8e7115886e2ae0954ffe40f8b3dc44
unknown <zulicic0@gmail.com>
2022-12-13 09:58:34 +0100

    Changed loading logic

--- src/components/reports/Reports.tsx
+++ src/components/reports/Reports.tsx
@@ -1,64 +1,27 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import ReportCard from './ReportCard';
 import Grid from '@mui/material/Grid';
-import { getAllReports } from '../../service/ReportService';
 import SomethingWentWrong from '../error/SomethingWentWrong';
+import { Report } from '../types/Types';
 
 interface Props {
-  // eslint-disable-next-line no-unused-vars
-  getLoaded: (b: boolean) => void;
+  error: boolean;
+  errorCode: string;
+  reports: Report[];
 }
 function Reports(props: Props) {
-  const [reports, setReports] = useState([]);
-  const [error, setError] = useState(false);
-  const [errorCode, setErrorCode] = useState('');
-
-  function handleGetAllReports(controller) {
-    getAllReports(controller)
-      .then((res) => {
-        setReports(res.data.reports);
-        props.getLoaded(true);
-      })
-      .catch((err) => {
-        {
-          if (err.code === 'ERR_CANCELED') {
-            console.log(err);
-            props.getLoaded(false);
-          } else {
-            console.log(err);
-            setError(true);
-            setErrorCode(err.code);
-            props.getLoaded(true);
-          }
-        }
-      });
-  }
-
-  const getReports = useCallback(async (controller) => {
-    handleGetAllReports(controller);
-  }, []);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    getReports(controller);
-
-    return () => {
-      controller.abort();
-    };
-  }, [getReports]);
-
   return (
     <div>
-      {!error ? (
+      {!props.error ? (
         <Grid container sx={{ flexGrow: 1 }} spacing={2}>
-          {reports.map((item, key) => (
+          {props.reports.map((item, key) => (
             <Grid item xs={12} md={4} key={key}>
               <ReportCard report={item} />
             </Grid>
           ))}
         </Grid>
       ) : (
-        <SomethingWentWrong errors={errorCode} />
+        <SomethingWentWrong errors={props.errorCode} />
       )}
     </div>
   );
--- src/components/reports/ReportsTabs.tsx
+++ src/components/reports/ReportsTabs.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Reports from './Reports';
 import TabContext from '@mui/lab/TabContext';
 import Box from '@mui/material/Box';
@@ -6,24 +6,56 @@ import TabList from '@mui/lab/TabList';
 import Tab from '@mui/material/Tab';
 import TabPanel from '@mui/lab/TabPanel';
 import LinearProgress from '@mui/material/LinearProgress';
+import { getAllReports } from '../../service/ReportService';
 
 function ReportsTabs() {
   const [value, setValue] = useState('1');
-  const [loaded, setLoaded] = useState(false);
+  const [reports, setReports] = useState([]);
+  const [error, setError] = useState(false);
+  const [errorCode, setErrorCode] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
-  function getLoaded(loaded: boolean) {
-    console.log(loaded);
-    setLoaded(loaded);
+  function handleGetAllReports(controller) {
+    getAllReports(controller)
+      .then((res) => {
+        setReports(res.data.reports);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        {
+          if (err.code === 'ERR_CANCELED') {
+            console.log(err);
+            setIsLoading(true);
+          } else {
+            console.log(err);
+            setError(true);
+            setErrorCode(err.code);
+            setIsLoading(false);
+          }
+        }
+      });
   }
+
+  const getReports = useCallback(async (controller) => {
+    handleGetAllReports(controller);
+  }, []);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    getReports(controller);
+
+    return () => {
+      controller.abort();
+    };
+  }, [getReports]);
 
   return (
     <div>
       <Box sx={{ width: '100%', typography: 'body1' }}>
-        {!loaded && (
+        {isLoading && (
           <Box sx={{ width: '100%' }}>
             <LinearProgress />
           </Box>
@@ -35,7 +67,7 @@ function ReportsTabs() {
             </TabList>
           </Box>
           <TabPanel value="1">
-            <Reports getLoaded={getLoaded} />
+            <Reports error={error} errorCode={errorCode} reports={reports} />
           </TabPanel>
         </TabContext>
       </Box>
