b7ba2afb24332aa8dce5626be48970d86403b76b
Amir Zulić <zulicic0@gmail.com>
2022-12-15 12:55:02 +0100

    Implemented contact form validation (#21)
    * Implemented contact form validation
    
    * Added message required
    
    * Separated warning description

--- src/components/contact/ContactUs.tsx
+++ src/components/contact/ContactUs.tsx
@@ -6,16 +6,17 @@ import emailjs from '@emailjs/browser';
 import { useNavigate } from 'react-router-dom';
 import Alert from '@mui/material/Alert';
 import Snackbar from '@mui/material/Snackbar';
-import { SEND_EMAIL } from '../../data/Data';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import Box from '@mui/material/Box';
 
 function ContactUs() {
   let navigate = useNavigate();
 
-  const [emailData, setEmailData] = useState(SEND_EMAIL);
   const [hasFailed, setHasFailed] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
 
-  function sendEmail() {
+  function sendEmail(emailData) {
     emailjs.send('service_eg2wfdo', 'template_agdnohr', emailData, 'APfvGvW3gFWJIaT4D').then(
       function () {
         setIsSuccess(true);
@@ -26,21 +27,57 @@ function ContactUs() {
     );
   }
 
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      name: '',
+      message: ''
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid email address').required('Required'),
+      message: Yup.string()
+        .max(50, 'The message can not be longer than 50 characters!')
+        .required('Required')
+    }),
+    onSubmit: () => {
+      const emailData = {
+        email: formik.values.email,
+        name: formik.values.name,
+        message: formik.values.message
+      };
+      sendEmail(emailData);
+    }
+  });
+
   return (
     <div>
-      <Snackbar
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-        open={hasFailed}
-        autoHideDuration={5000}>
-        <Alert severity="error" sx={{ width: '100%' }}>
-          There has been a problem!
-        </Alert>
-      </Snackbar>
+      {(formik.touched.email && formik.errors.email) ||
+      (formik.touched.message && formik.errors.message) ||
+      hasFailed ? (
+        <Snackbar
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+          open={true}
+          autoHideDuration={5000}>
+          <Alert severity={hasFailed ? 'error' : 'warning'} sx={{ width: '100%' }}>
+            {formik.touched.email && formik.values.email === ''
+              ? ' You need to provide us with your email.'
+              : null}
+            {formik.touched.message && formik.values.message === ''
+              ? ' The message field is required.'
+              : null}
+            {formik.values.email !== '' && formik.errors.email ? ' Check your email.' : null}
+            {formik.values.message !== '' && formik.errors.message
+              ? ' Your message is too long.'
+              : null}
+            {hasFailed && ' There has been a problem'}
+          </Alert>
+        </Snackbar>
+      ) : null}
       <br />
       <Grid container>
         <Grid item xs={2}></Grid>
         <Grid item xs={8}>
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <Grid container sx={{ flexGrow: 1 }} spacing={3}>
               <Grid item xs={12}>
                 <h1>Contact us</h1>
@@ -56,10 +93,13 @@ function ContactUs() {
                   variant="outlined"
                   size="small"
                   sx={{ width: 1 }}
-                  onChange={(e) => {
-                    setEmailData({ ...emailData, email: e.target.value });
-                  }}
+                  value={formik.values.email}
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
                 />
+                {formik.touched.email && formik.errors.email ? (
+                  <Box sx={{ color: 'error.main' }}>{formik.errors.email}</Box>
+                ) : null}
               </Grid>
               <Grid item xs={12}>
                 <TextField
@@ -69,9 +109,9 @@ function ContactUs() {
                   variant="outlined"
                   size="small"
                   sx={{ width: 1 }}
-                  onChange={(e) => {
-                    setEmailData({ ...emailData, name: e.target.value });
-                  }}
+                  value={formik.values.name}
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -84,15 +124,17 @@ function ContactUs() {
                   rows={4}
                   size="small"
                   sx={{ width: 1 }}
-                  onChange={(e) => {
-                    setEmailData({ ...emailData, message: e.target.value });
-                  }}
+                  value={formik.values.message}
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
                 />
+                {formik.touched.message && formik.errors.message ? (
+                  <Box sx={{ color: 'error.main' }}>{formik.errors.message}</Box>
+                ) : null}
               </Grid>
               <Grid item xs={12}>
                 <Button
-                  type="button"
-                  onClick={sendEmail}
+                  type="submit"
                   sx={{ backgroundColor: '#e8d7a7', color: 'text.secondary', borderRadius: 0 }}>
                   SUBMIT FORM
                 </Button>
